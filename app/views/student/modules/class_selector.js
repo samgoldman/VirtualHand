36877a5d8c622cd42c8d18f7db8f7db8fd143f0d
samgoldman <sgoldman216@gmail.com>
2020-06-24 17:40:29 -0600

    Breakout common sort code
    Signed-off-by: samgoldman <sgoldman216@gmail.com>

--- app/views/student/modules/class_selector.js
+++ app/views/student/modules/class_selector.js
@@ -9,26 +9,6 @@ function getSelectedClassId() {
 			return options[i].value;
 }
 
-function sortClasses() {
-	let selElem = document.getElementById("class_selector");
-	let tmpAry = [];
-	for (let i = 0; i < selElem.options.length; i++) {
-		tmpAry[i] = [];
-		tmpAry[i][0] = selElem.options[i].text;
-		tmpAry[i][1] = selElem.options[i].value;
-		tmpAry[i][2] = selElem.options[i].selected;
-	}
-	tmpAry.sort();
-	while (selElem.options.length > 0) {
-		selElem.options[0] = null;
-	}
-	for (let i = 0; i < tmpAry.length; i++) {
-		let op = new Option(tmpAry[i][0], tmpAry[i][1]);
-		op.selected = tmpAry[i][2];
-		selElem.options[i] = op;
-	}
-}
-
 window.addEventListener("load", function () {
 	document.getElementById("class_selector").addEventListener('change', ClassSelectorChanged);
 	sortClasses();
--- app/views/teacher/modules/class_selector.js
+++ app/views/teacher/modules/class_selector.js
@@ -61,26 +61,6 @@ function RenameClass(id, name) {
 	ClassSelectorChanged();
 }
 
-function sortClasses() {
-	let selElem = document.getElementById("class_selector");
-	let tmpAry = [];
-	for (let i = 0; i < selElem.options.length; i++) {
-		tmpAry[i] = [];
-		tmpAry[i][0] = selElem.options[i].text;
-		tmpAry[i][1] = selElem.options[i].value;
-		tmpAry[i][2] = selElem.options[i].selected;
-	}
-	tmpAry.sort();
-	while (selElem.options.length > 0) {
-		selElem.options[0] = null;
-	}
-	for (let i = 0; i < tmpAry.length; i++) {
-		let op = new Option(tmpAry[i][0], tmpAry[i][1]);
-		op.selected = tmpAry[i][2];
-		selElem.options[i] = op;
-	}
-}
-
 window.addEventListener("load", function () {
 	$('#class_selector').change(ClassSelectorChanged);
 
