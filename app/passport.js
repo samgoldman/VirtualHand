b1c80a7a9b61ae60709366edfef7b32e6f78074b
samgoldman <sgoldman216@gmail.com>
2020-06-24 21:06:27 -0600

    Reorganize passport.js: pullout functions, use await
    Signed-off-by: samgoldman <sgoldman216@gmail.com>

--- app/passport.js
+++ app/passport.js
@@ -12,57 +12,46 @@ let localStrategyOptions = {
 	passReqToCallback: true
 };
 
+// Given a user, serialize it to it's ID
+const serializeUser = (user, done) => done(null, user._id);
+
+// Given a user ID, find a user object
+const deserializeUser = async (id, done) => {
+	const user = await User.findById(id);
+	done(null, user);
+};
+
+const signupStrategy = async (req, username, password, done) => {
+	const user = await User.findOne({'username': username}).exec();
+
+	// Username is taken, do not continue
+	if (user)
+		return done(null, false, req.flash('signupMessage', 'That username is already taken.'));
+
+	// Create the user
+	const newUser = new User();
+	newUser.username = username;
+	newUser.password = newUser.generateHash(password);
+	newUser.email = req.body.email;
+	newUser.role = req.body.role;
+	done(null, await newUser.save());
+};
+
+const loginStrategy = async (req, username, password, done) => {
+	try {
+		const user = await User.findOne({'username': username}).exec();
+		if (!user || !user.validPassword(password))
+			return done(null, false, req.flash('loginMessage', 'Incorrect credentials'));
+		done(null, await user.save());
+	} catch (err) {
+		done(err);
+	}
+};
+
 // expose this function to our app using module.exports
 module.exports = function (passport) {
-
-	// used to serialize the user for the session
-	passport.serializeUser(function (user, done) {
-		done(null, user._id);
-	});
-
-	// used to deserialize the user
-	passport.deserializeUser(function (id, done) {
-		User.findById(id)
-			.then(function (user) {
-				done(null, user);
-			});
-	});
-
-	passport.use('local-signup', new LocalStrategy(localStrategyOptions, function (req, username, password, done) {
-		User.findOne({'username': username})
-			.then(function (user) {
-				if (user) return done(null, false, req.flash('signupMessage', 'That username is already taken.'));
-			})
-			.then(function () {
-				let newUser = new User();
-				newUser.username = username;
-				newUser.password = newUser.generateHash(password);
-				newUser.email = req.body.email;
-				if (req.body.role === 'teacher') {
-					newUser.role = 'teacher';
-				} else if (req.body.role === 'student') {
-					newUser.role = 'student';
-				}
-
-				return newUser.save();
-			})
-			.then(function (user) {
-				done(null, user);
-			});
-	}));
-
-	passport.use('local-login', new LocalStrategy(localStrategyOptions, function (req, username, password, done) {
-		User.findOne({'username': username})
-			.exec()
-			.then(function (user) {
-				if (!user || !user.validPassword(password)) {
-					return done(null, false, req.flash('loginMessage', 'Incorrect credentials'));
-				}
-				user.save();
-				return done(null, user);
-			})
-			.catch(function (err) {
-				done(err);
-			});
-	}));
+	passport.serializeUser(serializeUser);
+	passport.deserializeUser(deserializeUser);
+	passport.use('local-signup', new LocalStrategy(localStrategyOptions, signupStrategy));
+	passport.use('local-login', new LocalStrategy(localStrategyOptions, loginStrategy));
 };
