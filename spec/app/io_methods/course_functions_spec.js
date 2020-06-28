398184914d21dfb38a675909d8969e7c35f23ef3
samgoldman <sgoldman216@gmail.com>
2020-06-28 09:53:27 -0600

    Add positive test for createCourse
    Signed-off-by: samgoldman <sgoldman216@gmail.com>

--- spec/app/io_methods/course_functions_spec.js
+++ spec/app/io_methods/course_functions_spec.js
@@ -9,7 +9,7 @@ const mock_socket = {
 describe('Course Functions', () => {
 	describe('>createCourse', () => {
 		[undefined, null].forEach(courseName => {
-			it('not change the course name if the course name is undefined', async () => {
+			it(`not change the course name if the course name is ${courseName}`, async () => {
 				expect(createCourse).toBeDefined();
 
 				const test_user_id = 'abc123';
@@ -43,8 +43,47 @@ describe('Course Functions', () => {
 				});
 
 				expect(test_course._id).toEqual('424242');
-
 			});
+		});
+
+		it('not change the course name if the course name is undefined', async () => {
+			expect(createCourse).toBeDefined();
+
+			const test_user_id = 'abc123';
+
+			const test_user = {
+				_id: 'abc123'
+			};
+
+			const test_course = {
+				_id: '424242'
+			};
+
+			const spy_findById = spyOn(User, 'findById').and.returnValue(new Promise(done => done(test_user)));
+			const spy_create = spyOn(Course, 'create').and.returnValue(new Promise(done => done(test_course)));
+			const spy_emit = spyOn(mock_socket, 'emit').and.returnValue(undefined);
+
+			expect(await createCourse(mock_socket, test_user_id, 'random_course_name')).toEqual(undefined);
+
+			expect(spy_findById.calls.count()).toEqual(1);
+			expect(spy_findById.calls.argsFor(0).length).toEqual(1);
+			expect(spy_findById.calls.argsFor(0)[0]).toEqual(test_user_id);
+
+			expect(spy_create.calls.count()).toEqual(1);
+			expect(spy_create.calls.argsFor(0).length).toEqual(1);
+			expect(spy_create.calls.argsFor(0)[0]).toEqual({teacher: test_user_id, courseName: 'random_course_name'});
+
+			expect(spy_emit.calls.count()).toEqual(1);
+			expect(spy_emit.calls.argsFor(0).length).toEqual(2);
+			expect(spy_emit.calls.argsFor(0)[0]).toEqual('Response_CourseCreate');
+			expect(spy_emit.calls.argsFor(0)[1]).toEqual({
+				courseId: '424242',
+				courseName: 'random_course_name',
+				message: 'Class created successfully.',
+				success: true
+			});
+
+			expect(test_course._id).toEqual('424242');
 		});
 	});
 });
