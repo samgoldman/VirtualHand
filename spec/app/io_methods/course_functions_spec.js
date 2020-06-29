d2c29c2a9da41c3310a1f04bb7b0b8685bc3fecb
samgoldman <sgoldman216@gmail.com>
2020-06-28 18:47:37 -0600

    createCourse check if user ID is valid: test red
    Signed-off-by: samgoldman <sgoldman216@gmail.com>

--- spec/app/io_methods/course_functions_spec.js
+++ spec/app/io_methods/course_functions_spec.js
@@ -46,6 +46,38 @@ describe('Course Functions', () => {
 			});
 		});
 
+		it('not change the course name if the user ID is invalid', async () => {
+			expect(createCourse).toBeDefined();
+
+			const test_user_id = 'abc123';
+
+			const test_course = {
+				_id: '424242'
+			};
+
+			const spy_findById = spyOn(User, 'findById').and.returnValue(new Promise(done => done(undefined)));
+			const spy_create = spyOn(Course, 'create').and.returnValue(new Promise(done => done(test_course)));
+			const spy_emit = spyOn(mock_socket, 'emit').and.returnValue(undefined);
+
+			expect(await createCourse(mock_socket, test_user_id, 'test_course')).toEqual(undefined);
+
+			expect(spy_findById.calls.count()).toEqual(1);
+			expect(spy_findById.calls.argsFor(0).length).toEqual(1);
+			expect(spy_findById.calls.argsFor(0)[0]).toEqual(test_user_id);
+
+			expect(spy_create.calls.count()).toEqual(0);
+
+			expect(spy_emit.calls.count()).toEqual(1);
+			expect(spy_emit.calls.argsFor(0).length).toEqual(2);
+			expect(spy_emit.calls.argsFor(0)[0]).toEqual('Response_CourseCreate');
+			expect(spy_emit.calls.argsFor(0)[1]).toEqual({
+				success: false,
+				message: 'Class not created: user ID is invalid'
+			});
+
+			expect(test_course._id).toEqual('424242');
+		});
+
 		it('not change the course name if the course name is undefined', async () => {
 			expect(createCourse).toBeDefined();
 
