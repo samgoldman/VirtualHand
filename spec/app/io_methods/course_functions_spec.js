bd5bbfd0036c28d72230c50d3bd54cea04d91ce1
samgoldman <sgoldman216@gmail.com>
2020-06-28 21:26:50 -0600

    Add test for deleteCourse
    Signed-off-by: samgoldman <sgoldman216@gmail.com>

--- spec/app/io_methods/course_functions_spec.js
+++ spec/app/io_methods/course_functions_spec.js
@@ -1,10 +1,11 @@
 const Course = require('../../../app/models/course').model;
 const User = require('../../../app/models/user').model;
-const {createCourse, renameCourse} = require('../../../app/io_methods/course_functions');
+const Enrollment = require('../../../app/models/enrollment').model;
+const {createCourse, renameCourse, deleteCourse} = require('../../../app/io_methods/course_functions');
 
 const mock_socket = {
 	emit: () => {}
-}
+};
 
 describe('Course Functions', () => {
 	describe('>createCourse', () => {
@@ -220,6 +221,50 @@ describe('Course Functions', () => {
 
 			expect(test_course._id).toEqual('424242');
 			expect(test_course.courseName).toEqual('newCourseName');
+		});
+	});
+
+	describe('>deleteCourse', () => {
+		it('should not delete the course if the user does not teach it', async () => {
+			expect(deleteCourse).toBeDefined();
+
+			const mock_course_document_query = {
+				updateOne: () => new Promise(done => done(undefined))
+			};
+
+			const mock_enrollment_document_query = {
+				updateMany: () => new Promise(done => done(undefined))
+			};
+
+			const spy_findById = spyOn(Course, 'findById').and.returnValue(mock_course_document_query);
+			const spy_verifyCourseTaughtBy = spyOn(Course, 'verifyCourseTaughtBy').and.throwError('Teacher does not teach class!');
+			const spy_find = spyOn(Enrollment, 'find').and.returnValue(mock_enrollment_document_query);
+			const spy_emit = spyOn(mock_socket, 'emit').and.returnValue(undefined);
+			const spy_updateOne = spyOn(mock_course_document_query, 'updateOne').and.returnValue(new Promise(done => done(undefined)));
+			const spy_updateMany = spyOn(mock_enrollment_document_query, 'updateMany').and.returnValue(new Promise(done => done(undefined)));
+
+			expect(await deleteCourse(mock_socket, 'user-id', 'course-id')).toEqual(undefined);
+
+			expect(spy_verifyCourseTaughtBy.calls.count()).toEqual(1);
+			expect(spy_verifyCourseTaughtBy.calls.argsFor(0).length).toEqual(2);
+			expect(spy_verifyCourseTaughtBy.calls.argsFor(0)[0]).toEqual('course-id');
+			expect(spy_verifyCourseTaughtBy.calls.argsFor(0)[1]).toEqual('user-id');
+
+			expect(spy_findById.calls.count()).toEqual(0);
+
+			expect(spy_find.calls.count()).toEqual(0);
+
+			expect(spy_updateMany.calls.count()).toEqual(0);
+
+			expect(spy_updateOne.calls.count()).toEqual(0);
+
+			expect(spy_emit.calls.count()).toEqual(1);
+			expect(spy_emit.calls.argsFor(0).length).toEqual(2);
+			expect(spy_emit.calls.argsFor(0)[0]).toEqual('Response_DeleteCourse');
+			expect(spy_emit.calls.argsFor(0)[1]).toEqual({
+				success: false,
+				message: 'Teacher does not teach class!'
+			});
 		});
 	});
 });
