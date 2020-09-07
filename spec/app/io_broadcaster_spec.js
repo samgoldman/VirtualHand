0d3836d74d44670697f782859b3e3807a96a8cfc
Sam Goldman <sgoldman216@gmail.com>
2020-09-06 18:17:22 -0600

    Test broadcast globablly

--- spec/app/io_broadcaster_spec.js
+++ spec/app/io_broadcaster_spec.js
@@ -4,11 +4,33 @@ const io_broadcaster = rewire('../../app/io_broadcaster');
 describe('io_broadcaster', () => {
     describe('>init', () => {
         it('should change global_io from null to the provided value', () => {
+            expect(io_broadcaster.init).toBeDefined();
             expect(io_broadcaster.__get__('global_io')).toBeNull();
-
             expect(io_broadcaster.init('test_value')).toBeUndefined();
-
             expect(io_broadcaster.__get__('global_io')).toEqual('test_value');
+        });
+    });
+
+    describe('>broadcastGlobally', () => {
+        it('should broadcast the given event and data to the entire server', () => {
+            expect(io_broadcaster.broadcastGlobally).toBeDefined();
+
+            const mock_io = {
+                emit: () => undefined
+            };
+
+            io_broadcaster.__set__('global_io', mock_io);
+
+            const spy_emit = spyOn(mock_io, 'emit').and.callThrough();
+
+            expect(io_broadcaster.broadcastGlobally('test_event', {message: 'test_message'})).toBeUndefined();
+
+            expect(spy_emit.calls.count()).toEqual(1);
+            expect(spy_emit.calls.argsFor(0).length).toEqual(2);
+            expect(spy_emit.calls.argsFor(0)[0]).toEqual('test_event');
+            expect(spy_emit.calls.argsFor(0)[1]).toEqual({message: 'test_message'});
+
+            io_broadcaster.__set__('global_io', null);
         });
     });
 });
