0fdb2368dfb54333937a74a82525fc505f500d86
Sam Goldman <sgoldman216@gmail.com>
2020-09-06 19:02:25 -0600

    Update change password module

--- app/views/shared/change_password_module.js
+++ app/views/shared/change_password_module.js
@@ -1,23 +1,32 @@
-function ChangePassword() {
-	let data = {
-		oldPassword: document.getElementById('oldPassword').value,
-		newPassword: document.getElementById('newPassword').value
+const ChangePassword = () => {
+	const data = {
+		oldPassword: document.querySelector("#old_password").value,
+		newPassword: document.querySelector("#new_password").value
 	};
 	socket.emit('Request_PasswordChange', data, ChangePasswordCallback);
 }
 
-function ChangePasswordCallback(data) {
-	document.getElementById("change_password_alert_box").innerHTML = data.message;
-	document.getElementById("change_password_alert_box").style.display = "block";
+const ChangePasswordCallback = data => {
+	const change_password_alert_box = document.querySelector("#change_password_alert_box");
+	change_password_alert_box.innerHTML = data.message;
+	change_password_alert_box.style.display = "block";
 }
 
-window.addEventListener("load", function () {
-	document.getElementById("changePasswordSubmit").addEventListener('click', ChangePassword);
+const ClearChangePasswordModal = () => {
+	const change_password_alert_box = document.querySelector("#change_password_alert_box");
+	const new_password = document.querySelector('#new_password');
+	const old_password = document.querySelector('#old_password');
 
-	$(".change-password-modal").on("hidden.bs.modal", function(){
-		document.getElementById("change_password_alert_box").innerHTML = '';
-		document.getElementById("change_password_alert_box").style.display = "none";
-		$('#oldPassword').val('');
-		$('#newPassword').val('');
-	});
-});
+	change_password_alert_box.innerHTML = '';
+	change_password_alert_box.style.display = "none";
+
+	new_password.value = '';
+	old_password.value = '';
+};
+
+const ChangePasswordModuleInit = () => {
+	document.querySelector("#changePasswordSubmit").addEventListener("click", ChangePassword);
+	$(".change-password-modal").on("hidden.bs.modal", ClearChangePasswordModal);
+}
+
+window.addEventListener("load", ChangePasswordModuleInit);
