9b180b5b0178f68a373629a5df32fba5bdf5e557
Sam Goldman <sgoldman216@gmail.com>
2020-09-09 20:51:56 -0600

    Update class_selector.js

--- app/views/student/modules/class_selector.js
+++ app/views/student/modules/class_selector.js
@@ -1,15 +1,16 @@
-function ClassSelectorChanged() {
-	sortClasses();
-}
+// Unclear why this function just wraps sortClasses, but 'ClassSelectorChanged' is overloaded witha teacher version
+const ClassSelectorChanged = () => sortClasses();
 
-function getSelectedClassId() {
+const getSelectedClassId = () => {
 	let options = document.getElementById("class_selector").getElementsByTagName("option");
 	for (let i = options.length; i--;)
 		if (options[i].selected)
 			return options[i].value;
-}
+};
 
-window.addEventListener("load", function () {
+const ClassSelectorInit = () => {
 	document.getElementById("class_selector").addEventListener('change', ClassSelectorChanged);
 	sortClasses();
-});
+};
+
+window.addEventListener("load", ClassSelectorInit);
