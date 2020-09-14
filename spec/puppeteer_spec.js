eb9a95d8da862f85aeba2c538bbbeb2db954b6cd
Sam Goldman <sgoldman216@gmail.com>
2020-09-14 07:39:48 -0600

    Testing puppeteer

---
+++ spec/puppeteer_spec.js
@@ -0,0 +1,16 @@
+const { EXIT_CODE } = require('karma/lib/constants');
+const puppeteer = require('puppeteer');
+const server = require('../server');
+
+describe('test', () => {
+
+it('should have a title', async () => {
+
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('http://localhost:8080');
+    expect(await page.title()).toEqual('Virtual Hand Login');
+    await browser.close();
+
+});
+})
