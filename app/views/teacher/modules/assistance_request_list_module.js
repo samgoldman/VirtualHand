9fdb7c3036d8bdffb2daa5ecdb3594bcc304e0a0
Sam Goldman <sgoldman216@gmail.com>
2020-09-30 22:46:09 -0600

    Refactor KeyDownHandler

--- app/views/teacher/modules/assistance_request_list_module.js
+++ app/views/teacher/modules/assistance_request_list_module.js
@@ -40,27 +40,10 @@ const ClearAllAssistanceRequests = () => {
 }
 
 const KeyDownHandler = (e) => {
-	let keynum;
+	const key = window.event !== undefined ? String.fromCharCode(e.keyCode) : String.fromCharCode(e.which);
 
-	if (window.event) { // IE
-		keynum = e.keyCode;
-	} else if (e.which) { // Netscape/Firefox/Opera
-		keynum = e.which;
-	}
-	let key = String.fromCharCode(keynum);
-	if (key === '1') {
-		handDown(0);
-	} else if (key === '2') {
-		handDown(1);
-	} else if (key === '3') {
-		handDown(2);
-	} else if (key === '4') {
-		handDown(3);
-	} else if (key === '5') {
-		handDown(4);
-	} else if (key === 'r') {
-		RequestRandomStudent(getSelectedClassId());
-	}
+	if (false === isNaN(parseInt(key))) handDown(parseInt(key) - 1);
+	else if (key === 'r') RequestRandomStudent(getSelectedClassId());
 }
 
 const AssistanceRequestListModuleInit = () => {
