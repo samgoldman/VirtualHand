4212272cf8a55a979e5eb4d9123f7bd8a2f26caa
Sam Goldman <sgoldman216@gmail.com>
2020-10-02 13:29:05 -0600

    Add tests

--- test_frontend/app/views/teacher/modules/hall_pass_module_spec.js
+++ test_frontend/app/views/teacher/modules/hall_pass_module_spec.js
@@ -23,5 +23,69 @@ define((require) => {
                 });
             })
         });
+
+        describe('>resolvePass', () => {
+            it('should be defined', () => {
+                expect(resolvePass).toBeDefined();
+            });
+
+            it('should send a request to the server with the provided id', () => {
+                const mock_socket = {
+                    emit: () => undefined
+                };
+                socket = mock_socket;
+
+                const spy_emit = spyOn(mock_socket, 'emit').and.stub();
+
+                expect(resolvePass('test_hall_pass_id_24')).toBeUndefined();
+
+                expect(spy_emit.calls.count()).toEqual(1);
+                expect(spy_emit.calls.argsFor(0)).toEqual(['Request_TeacherResolveHallPassRequest', {hrid: 'test_hall_pass_id_24'}]);
+            });
+        })
+
+        describe('>grantPass', () => {
+            it('should be defined', () => {
+                expect(grantPass).toBeDefined();
+            });
+
+            it('should send a request to the server with the provided id', () => {
+                const mock_socket = {
+                    emit: () => undefined
+                };
+                socket = mock_socket;
+
+                const spy_emit = spyOn(mock_socket, 'emit').and.stub();
+
+                expect(grantPass('test_hall_pass_id_123')).toBeUndefined();
+
+                expect(spy_emit.calls.count()).toEqual(1);
+                expect(spy_emit.calls.argsFor(0)).toEqual(['Request_TeacherGrantHallPassRequest', {hrid: 'test_hall_pass_id_123'}]);
+            });
+        })
+
+        describe('>RetrieveHallPassRequests', () => {
+            it('should be defined', () => {
+                expect(RetrieveHallPassRequests).toBeDefined();
+            });
+
+            it('should send a request to the server with the provided id', () => {
+                const mock_socket = {
+                    emit: () => undefined
+                };
+                socket = mock_socket;
+
+                const spy_emit = spyOn(mock_socket, 'emit').and.stub();
+                const spy_getSelectedClassIds = spyOn(window, 'getSelectedClassIds').and.returnValue(['a', '1', 'b']);
+
+                expect(RetrieveHallPassRequests()).toBeUndefined();
+
+                expect(spy_emit.calls.count()).toEqual(1);
+                expect(spy_emit.calls.argsFor(0)).toEqual(['Request_RetrieveHallPassRequests', {cids: ['a', '1', 'b']}]);
+
+                expect(spy_getSelectedClassIds.calls.count()).toEqual(1);
+                expect(spy_getSelectedClassIds.calls.argsFor(0)).toEqual([]);
+            });
+        });
     });
 });
